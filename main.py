8603767a44f323183411cec37226c232fe53e153
MuhammadMoaz <muhammadmoaz2020@outlook.com>
2025-07-05 00:35:57 +1000

    added basic add_task functionality

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
+import json
+import re
+
 def add_task(task_name):
     # open JSON for writing
     # assign ID to task
     # store task and relevant details to JSON
     # output confirmation
     id = 1
-    print("Task Added Successfully (ID: {id}, Name: {task_name})")
+    print(f"Task Added Successfully (ID: {id}, Name: {task_name})")
 
+def main():
+    while True:
+        # Get input
+        command = input('task-cli:')
+        # Get operation from input
+        operation = command.split('"')
+        print(operation)
+        # Match command using regex
+        match operation[0].strip():
+            case "add":
+                add_task(operation[1])
+            case "update":
+                print("task update")
+            case "delete":
+                print("task deleted")
+            case "mark-in-progress":
+                print("task marked as in progress")
+            case "mark-done":
+                print("task marked as done")
+            case "list":
+                print("listing tasks")
+            case "quit":
+                print("quitting...")
+                exit()
 
-while True:
-    command = input('task-cli:')
-    operation, parameter = command.split(' ')
-    match command:
-        case "add":
-            add_task(parameter)
-        case "update":
-            print("task update")
-        case "delete":
-            print("task deleted")
-        case "mark-in-progress":
-            print("task marked as in progress")
-        case "mark-done":
-            print("task marked as done")
-        case "list":
-            print("listing tasks")
-        case "quit":
-            print("quitting...")
-            exit()
-    
+main()
