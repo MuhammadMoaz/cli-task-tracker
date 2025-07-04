09cbd0f9140baf60d3c72f3123ab6a99a6fa17bb
MuhammadMoaz <muhammadmoaz2020@outlook.com>
2025-07-05 00:39:34 +1000

    added hi

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def add_task(task_name):
     print(f"Task Added Successfully (ID: {id}, Name: {task_name})")
 
 def main():
+    print("HI!")
     while True:
         # Get input
         command = input('task-cli:')
